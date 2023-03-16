d1966882059fe4a5e4d04eed4abc82b97b4abc23
lokesh-122 <94118308+lokesh-122@users.noreply.github.com>
2023-03-16 07:56:37 +0530

    Update View code.py

--- View code.py
+++ View code.py
@@ -1,76 +1,205 @@
-n=int(input())
 print("Welcome to view point")
+print("Press any alphabet to print the programs list")
+n=eval(input("Enter the number for perfroming the specific program"))
 if n==1:
   print("")
-if n==2:
+elif n==2:
   print("")
-if n==3:
+elif n==3:
   print("")
-if n==4:
+elif n==4:
   print("")
-if n==5:
+elif n==5:
   print("")
-if n==6:
+elif n==6:
   print("")
-if n==7:
+elif n==7:
   print("")
-if n==8:
+elif n==8:
   print("")
-if n==9:
+elif n==9:
   print("")
-if n==10:
+elif n==10:
   print("")
-if n==11:
+elif n==11:
   print("")
-if n==12:
+elif n==12:
   print("")
-if n==13:
+elif n==13:
   print("")
-if n==14:
+elif n==14:
   print("")
-if n=15:
+elif n==15:
   print("")
-if n==16:
+elif n==16:
   print("")
-if n==17:
+elif n==17:
   print("")
- if n==18:
+elif n==18:
   print("")
- if n==19:
+elif n==19:
   print("")
- if n==20:
+elif n==20:
   print("")
- if n==21:
+elif n==21:
   print("")
- if n==22:
+elif n==22:
   print("")
- if n==19:
+elif n==23:
   print("")
- if n==19:
+elif n==24:
   print("")
- if n==19:
+elif n==25:
   print("")
- if n==19:
+elif n==26:
   print("")
- if n==19:
+elif n==27:
   print("")
- if n==19:
+elif n==28:
   print("")
- if n==19:
+elif n==29:
   print("")
- if n==19:
+elif n==30:
   print("")
- if n==19:
+elif n==31:
   print("")
- if n==19:
+elif n==32:
   print("")
- if n==19:
+elif n==33:
   print("")
-if n==19:
+elif n==34:
   print("")
- if n==19:
+elif n==35:
   print("")
- if n==19:
+elif n==36:
   print("")
- if n==19:
+elif n==38:
   print("")
+elif n==39:
+  print("")
+elif n==40:
+  print("")
+elif n==41:
+  print("")
+elif n==42:
+  print("")
+elif n==43:
+  print("")
+elif n==44:
+  print("")
+elif n==45:
+  print("")
+elif n==46:
+  print("")
+elif n==47:
+  print("")
+elif n==48:
+  print("")
+elif n==49:
+  print("")
+elif n==50:
+  print("")
+elif n==51:
+  print("")
+elif n==52:
+  print("")
+elif n==53:
+  print("")
+elif n==54:
+  print("")
+elif n==55:
+  print("")
+elif n==56:
+  print("")
+elif n==57:
+  print("")
+elif n==58:
+  print("")
+elif n==59:
+  print("")
+elif n==60:
+  print("")
+elif n==61:
+  print("")
+elif n==62:
+  print("")
+elif n==63:
+  print("")
+elif n==64:
+  print("")
+elif n==65:
+  print("")
+elif n==66:
+  print("")
+elif n==67:
+  print("")
+elif n==68:
+  print("")
+elif n==69:
+  print("")
+elif n==70:
+  print("")
+elif n==71:
+  print("")
+elif n==72:
+  print("")
+elif n==73:
+  print("")
+elif n==74:
+  print("")
+elif n==75:
+  print("")
+elif n==76:
+  print("")
+elif n==77:
+  print("")
+elif n==78:
+  print("")
+elif n==79:
+  print("")
+elif n==80:
+  print("")
+elif n==81:
+  print("")
+elif n==82:
+  print("")
+elif n==83:
+  print("")
+elif n==84:
+  print("")
+elif n==85:
+  print("")
+elif n==86:
+  print("")
+elif n==87:
+  print("")
+elif n==88:
+  print("")
+elif n==89:
+  print("")
+elif n==90:
+  print("")
+elif n==91:
+  print("")
+elif n==92:
+  print("")
+elif n==93:
+  print("")
+elif n==94:
+  print("")
+elif n==95:
+  print("")
+elif n==96:
+  print("")
+elif n==97:
+  print("")
+elif n==98:
+  print("")
+elif n==99:
+  print("")
+elif n==100:
+  print("")
+else:
+  print("The list of programs are:")
+  print("")
+
