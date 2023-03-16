b69e789f4749f1756225e9d754d033797cf80238
lokesh-122 <94118308+lokesh-122@users.noreply.github.com>
2023-03-16 06:06:26 +0530

    Update View code.py

--- View code.py
+++ View code.py
@@ -1,1 +1,76 @@
-
+n=int(input())
+print("Welcome to view point")
+if n==1:
+  print("")
+if n==2:
+  print("")
+if n==3:
+  print("")
+if n==4:
+  print("")
+if n==5:
+  print("")
+if n==6:
+  print("")
+if n==7:
+  print("")
+if n==8:
+  print("")
+if n==9:
+  print("")
+if n==10:
+  print("")
+if n==11:
+  print("")
+if n==12:
+  print("")
+if n==13:
+  print("")
+if n==14:
+  print("")
+if n=15:
+  print("")
+if n==16:
+  print("")
+if n==17:
+  print("")
+ if n==18:
+  print("")
+ if n==19:
+  print("")
+ if n==20:
+  print("")
+ if n==21:
+  print("")
+ if n==22:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
+ if n==19:
+  print("")
